a78a97e80deacd3bb0b0c2267befdb446cca7650
Syeda Sarah Parvin <sarahsuhana2211@gmail.com>
2026-03-15 08:30:16 +0530

    Update Book.java

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,4 +1,5 @@
 package model;
+import java.util.ArrayList;
 
 public abstract class Book {
 
@@ -7,6 +8,7 @@ public abstract class Book {
     private int totalPages;
     private int pagesRead;
     private String status; // Planned, Reading, Completed
+    private ArrayList<Character> characters;
 
     public Book(String title, String author, int totalPages) {
         this.title = title;
@@ -14,9 +16,16 @@ public abstract class Book {
         this.totalPages = totalPages;
         this.pagesRead = 0;
         this.status = "Planned";
+        this.characters = new ArrayList<>();
     }
 
     public void updateProgress(int pages) {
+        
+        if (pages < 0 || pages > totalPages) {
+            System.out.println("Invalid page number.");
+            return;
+        }
+        
         pagesRead = pages;
 
         if (pagesRead >= totalPages) {
@@ -25,6 +34,36 @@ public abstract class Book {
             status = "Reading";
         }
     }
+
+    //multiverse characters-------------------------------------------------------
+    public void addCharacter(Character character) {
+        characters.add(character);
+    }
+
+    public void showCharacters() {
+
+        if (characters.isEmpty()) {
+            System.out.println("No characters listed.");
+            return;
+        }
+
+        System.out.println("Characters:");
+
+        for (Character c : characters) {
+            System.out.println("- " + c.getName());
+        }
+    }
+
+    public void displayInfo() {
+
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Progress: " + pagesRead + "/" + totalPages);
+        System.out.println("Status: " + status);
+
+        showCharacters();
+    }
+    //---------------------------------------------------------------------------------
 
     public String getTitle() {
         return title;
