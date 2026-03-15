4aaf2c6d05072bd93f0c0f2be20fd8aeb0340efe
Syeda Sarah Parvin <sarahsuhana2211@gmail.com>
2026-03-15 08:21:24 +0530

    Update Universe.java

--- src/model/Universe.java
+++ src/model/Universe.java
@@ -6,15 +6,36 @@ public class Universe {
 
     private String name;
     private Hashmap<Series> seriesList;
+    private HashMap<String, Universe> connectedUniverses;
 
     public Universe(String name) {
         this.name = name;
         seriesList = new Hashmap<>();
+        connectedUniverses = new HashMap<>();
     }
 
     public void addSeries(Series series) {
         seriesList.put(series.getName(), series);
     }
+
+    //entangled universes-----------------------------------------
+    public void connectUniverse(Universe universe) {
+        connectedUniverses.put(universe.getName(), universe);
+    }
+
+    public void showConnectedUniverses() {
+
+        if (connectedUniverses.isEmpty()) {
+            System.out.println("No connected universes.");
+            return;
+        }
+
+        System.out.println("Connected Universes:");
+        for (Universe u : connectedUniverses.values()) {
+            System.out.println("- " + u.getName());
+        }
+    }
+    //---------------------------------------------------------------
 
     public Series getSeries(String name) {
         return seriesList.get(name);
